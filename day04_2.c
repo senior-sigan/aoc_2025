44ac12ec4283507ddf22ceda949324fd39d4ef5f
Ilya Siganov <ilya.blan4@gmail.com>
2025-12-11 00:35:40 +0600

    add day04_2

---
+++ day04_2.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define WIDTH  140
+#define HEIGHT 140
+
+char matrix[HEIGHT][WIDTH + 1] = {0};
+
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+
+int count(int x, int y) {
+    int c = 0;
+    for (int i = max(x - 1, 0); i <= min(x + 1, WIDTH-1); i++) {
+        for (int j = max(y - 1, 0); j <= min(y + 1, HEIGHT-1); j++) {
+            if (matrix[j][i] == '@') {
+                c++;
+            }
+        }
+    }
+    return c;
+}
+
+int remove_rolls() {
+    int counter = 0;
+    for (int j = 0; j < HEIGHT; j++) {
+        for (int i = 0; i < WIDTH; i++) {
+            char ch = matrix[j][i];
+            if (ch == '@') {
+                int c = count(i, j);
+                if (c <= 4) {
+                    counter++;
+                    matrix[j][i] = '.';
+                    continue;
+                }
+            }
+        }
+    }
+    return counter;
+}
+
+void printmatrix() {
+    printf("=========================\n");
+    for (int j = 0; j < HEIGHT; j++) {
+        for (int i = 0; i < WIDTH; i++) {
+            printf("%c", matrix[j][i]);
+        }
+        printf("\n");
+    }
+}
+
+int main(void) {
+    for (int j = 0; j < HEIGHT; j++) {
+        scanf("%s", matrix[j]);
+    }
+    printmatrix();
+
+    int counter = 0;
+    while (true) {
+        int c = remove_rolls();
+        counter += c;
+        printmatrix();
+        if (c==0) break;
+    }
+
+    printf("RESULT: %d\n", counter);
+
+    return 0;
+}
